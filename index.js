5e6b1cfc7788d82a6173c95f22500a28365d259c
Francis Mendoza <fm5150@yahoo.com>
2024-02-04 00:53:42 -0800

    Updated some comments

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const debug = false;
 
 // Assignment Task #1
+/*
+    Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+    Programmatically subtract the value of the first element in the array from the value in the last element of the array.
+    Do not use numbers to reference the last element, find it programmatically.
+    ages[7] - ages[0] is not allowed!
+    Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
+    Use a loop to iterate through the array and calculate the average age.
+*/
 printTaskNumber(1);
 
 let ages = [ 3, 9, 23, 64, 2, 8, 28, 93 ];
@@ -51,6 +59,11 @@ deBugger(`Sum total of ages in array = ${sumOfAges(ages)}`); // debug output. Ch
 console.log(sumOfAges(ages) / ages.length) // Average age from array
 
 // Assignment Task #2
+/*
+    Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.
+    Use a loop to iterate through the array and calculate the average number of letters per name.
+    Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
+*/ 
 printTaskNumber(2);
 
 let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
